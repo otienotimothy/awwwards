8e3f8f38a5f33b784ff730cc5cbf42226a2f18c9
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-04-17 19:29:25 +0300

    Add class to define login form input elements

--- awwrdsApp/forms.py
+++ awwrdsApp/forms.py
@@ -18,3 +18,14 @@ class UserRegistrationForm(UserCreationForm):
         super().__init__(*args, **kwargs)
         self.fields['password1'].widget.attrs['class'] = 'form-control'
         self.fields['password2'].widget.attrs['class'] = 'form-control'
+
+
+class LoginUserForm(forms.Form):
+
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control'}), max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password'].widget.attrs['class'] = 'form-control'
