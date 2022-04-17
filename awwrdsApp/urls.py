b7c170ef71ed266bba9651f1d3b4559ae63140d5
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-04-17 17:48:56 +0300

    Add route to upload project

--- awwrdsApp/urls.py
+++ awwrdsApp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import home, projectDetails, userprofile
+from .views import home, projectDetails, userprofile, uploadProject
 
 
 urlpatterns = [
     path('', home, name='home'),
     path('project/', projectDetails, name='projectDetails'),
-    path('profile/', userprofile, name='profile')
+    path('profile/', userprofile, name='profile'),
+    path('upload/', uploadProject, name='upload')
 ]
