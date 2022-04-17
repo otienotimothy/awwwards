362f0e62b3c97d0fdacd98f50ccd759a7b987b8e
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-04-17 16:28:45 +0300

    Add a route to for project details

--- awwrdsApp/urls.py
+++ awwrdsApp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import home
+from .views import home, projectDetails
 
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('project', projectDetails, name='projectDetails')
 ]
