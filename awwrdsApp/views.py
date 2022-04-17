9f27b4626f4ff1efdc37ca832f7f635cfc302f1b
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-04-17 19:27:30 +0300

    Add functionality to create a new user

--- awwrdsApp/views.py
+++ awwrdsApp/views.py
@@ -1,12 +1,34 @@
-from multiprocessing import context
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 from .forms import UserRegistrationForm
 
 
 # Create your views here.
 
 def signupUser(request):
+
+    if request.user.is_authenticated:
+        redirect('home')
+
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        user = form.save(commit=False)
+        if form.is_valid():
+            if User.objects.filter(username=user.username.lower()).exists():
+                messages.error(
+                    request, f'A User with the username, {user.username}, Already exists')
+            elif User.objects.filter(email=user.email.lower()).exists():
+                messages.error(
+                    request, f'A user with the email, {user.email}, Already exists')
+            else:
+                user.email = user.email.lower()
+                user.username = user.username.lower()
+                user.save()
+                login(request, user)
+                return redirect('home')
+            
 
     form = UserRegistrationForm()
     context = {'form': form}
