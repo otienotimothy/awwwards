12bb5d170cdf50376388251c2c69f89823188a88
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-04-17 19:17:38 +0300

    Define a class to define to sign up form inputs

---
+++ awwrdsApp/forms.py
@@ -0,0 +1,20 @@
+from dataclasses import fields
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'})
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
--- awwrdsApp/views.py
+++ awwrdsApp/views.py
@@ -1,9 +1,16 @@
+from multiprocessing import context
 from django.shortcuts import render
+
+from .forms import UserRegistrationForm
+
 
 # Create your views here.
 
 def signupUser(request):
-    return render(request, 'signup.html')
+
+    form = UserRegistrationForm()
+    context = {'form': form}
+    return render(request, 'signup.html', context)
 
 def loginUser(request):
     return render(request, 'login.html')
