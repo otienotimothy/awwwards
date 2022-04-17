34e5dccd1071a2a9c1c5847d8c9c3d67616403e2
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-04-17 18:08:58 +0300

    Add login and signup routes

--- awwrdsApp/urls.py
+++ awwrdsApp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from .views import home, projectDetails, userprofile, uploadProject
+from .views import home, projectDetails, userprofile, uploadProject, loginUser, signupUser
 
 
 urlpatterns = [
     path('', home, name='home'),
     path('project/', projectDetails, name='projectDetails'),
     path('profile/', userprofile, name='profile'),
-    path('upload/', uploadProject, name='upload')
+    path('upload/', uploadProject, name='upload'),
+    path('signup/', signupUser, name='signup'),
+    path('login/', loginUser, name='login')
 ]
